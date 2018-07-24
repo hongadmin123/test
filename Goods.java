4839115b770323e4ac01bcec2de5cc1ad8e8badd
hongadmin123 <41617291+hongadmin123@users.noreply.github.com>
2018-07-24 17:51:26 +0800

    更换目录
    上传文件

---
+++ Goods.java
@@ -0,0 +1,26 @@
+package com.po;
+
+public class Goods {
+private String gid;
+private String gname;
+private Float gprice;
+
+public String getGid() {
+	return gid;
+}
+public void setGid(String gid) {
+	this.gid = gid;
+}
+public String getGname() {
+	return gname;
+}
+public void setGname(String gname) {
+	this.gname = gname;
+}
+public Float getGprice() {
+	return gprice;
+}
+public void setGprice(Float gprice) {
+	this.gprice = gprice;
+}
+}
